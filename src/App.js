bf144ac652acd9160e84d01d1e1ce67cee0a5cbe
clarefausty <clarefausty@gmail.com>
2023-09-03 13:42:08 +0100

    Modified index.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState} from 'react';
 import axios from 'axios';
+
+
 
 function App() {
   const[data, setData] = useState('')
   const[location, setLocation] = useState('')
-
-
-  
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`
   
   const searchLocation = (event) =>{
@@ -18,6 +17,7 @@ function App() {
       setLocation("")
     }
     }
+    
     
 
   return (
